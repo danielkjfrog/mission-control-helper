8a5e082da2c3d901212921650951defa802ac406
Daniel Keler <danielk@jfrog.com>
2016-03-14 22:11:31 +0200

    Update populate.py

--- populate.py
+++ populate.py
@@ -3,7 +3,7 @@ import os
 import re
 
 def getInstancesUrls():
-    regex = re.compile('ARTIFACTORY._PORT')
+    regex = re.compile('ARTIFACTORY\d+_PORT=tcp:\/\/\d+.\d+.\d+.\d+:\d+')
     instances_keys = [instance for instance in os.environ if re.match(regex, instance)]
     instances_urls = [os.environ[instance_key].replace('tcp', 'http') for instance_key in instances_keys]
     return instances_urls
